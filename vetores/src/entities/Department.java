29cc50de28dc4d38474d736fd81345ab1796db9b
Vanessa de Freitas Ferreira <vanessafreitasferreir4@gmail.com>
2025-09-12 23:30:02 -0400

    composição

---
+++ vetores/src/entities/Department.java
@@ -0,0 +1,28 @@
+package entities;
+
+/**
+ * Autor: Vanessa de Freitas Ferreira
+ * Data: 12/09/2025
+ * Projeto: SeuProjetoAqui
+ * Descrição:
+ */
+
+public class Department {
+    private String name;
+
+    public Department() {
+    }
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
---
+++ vetores/src/enums/WorkerLevel.java
@@ -0,0 +1,12 @@
+package enums;
+
+/**
+ * @author vanessa
+ * @date 12/09/2025
+ * @description
+ */
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
